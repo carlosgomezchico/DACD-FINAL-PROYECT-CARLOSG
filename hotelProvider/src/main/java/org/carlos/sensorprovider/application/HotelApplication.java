d95c27d2be3b256f2e6212aaf14e214e8333972a
carlosgomezchico <carlosgomezchico11@gmail.com>
2024-06-24 16:30:59 +0100

    Modified HotelApplication class

---
+++ hotelProvider/src/main/java/org/carlos/sensorprovider/application/HotelApplication.java
@@ -0,0 +1,31 @@
+package org.carlos.sensorprovider.application;
+
+import org.carlos.sensorprovider.domain.Hotel;
+import org.carlos.sensorprovider.domain.Rate;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelApplication {
+    private final HotelProvider xoteloProvider;
+    private final ArrayList<Hotel> hotels;
+    private final HotelPublisher publisher;
+
+    public HotelApplication(HotelProvider hotelProvider, ArrayList<Hotel> hotels, HotelPublisher publisher) {
+        this.xoteloProvider = hotelProvider;
+        this.hotels = hotels;
+        this.publisher = publisher;
+    }
+    public void execute() {
+        LocalDate currentDate = LocalDate.now();
+        for (Hotel hotel : hotels) {
+            List<Rate> hotelRates = xoteloProvider.getHotelRates(hotel.getHotelKey(),currentDate);
+            Instant instant = Instant.now();
+            Hotel event = new Hotel(instant, "hotel-provider",hotel.getName(),hotel.getHotelKey(),
+                    hotel.getLocation(), hotelRates, currentDate.toString(),currentDate.plusDays(5).toString());
+            publisher.publish(event);
+        }
+    }
+}
