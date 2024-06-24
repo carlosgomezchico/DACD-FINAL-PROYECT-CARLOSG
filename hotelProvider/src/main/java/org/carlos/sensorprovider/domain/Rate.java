684fcd5f130168ca3c9b5d7df0342c60840d2d10
carlosgomezchico <carlosgomezchico11@gmail.com>
2024-06-24 16:38:28 +0100

    Modified Rate class

---
+++ hotelProvider/src/main/java/org/carlos/sensorprovider/domain/Rate.java
@@ -0,0 +1,28 @@
+
+package org.carlos.sensorprovider.domain;
+
+public class Rate {
+    private double price;
+    private String platformName;
+
+    public Rate(double price, String platformName) {
+        this.price = price;
+        this.platformName = platformName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPlatformName() {
+        return platformName;
+    }
+
+    public void setPlatformName(String platformName) {
+        this.platformName = platformName;
+    }
+}
